6681aedf3e6560cc704a66e7dcc6cbbe23c36478
rodzelte <zythonix.te@gmail.com>
2024-10-14 01:07:33 -0400

    2nd commit backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,114 +1,126 @@
-import {User} from '../models/user.model.js';
-import bcryptjs from "bcryptjs";
-import { generateTokenAndSetCooke } from '../utils/generateToken.js';
+import { User } from "../models/user.model.js"
+import bcryptjs from "bcryptjs"
+import { generateTokenAndSetCooke } from "../utils/generateToken.js"
 
-export async function signup(req,res){
-    try {
-       const {email,password,username} = req.body;
+export async function signup(req, res) {
+  try {
+    const { email, password, username } = req.body
 
-       if(!email || !password || !username){
-        return res.status(400).json({success:false,message:"All fields are required"});
-       }
-
-       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-       if(!emailRegex.test(email)){
-        return res.status(400).json({success:false,message:"Invalid email"});
-       }
-
-       if(password.length < 6){
-        return res.status(400).json({success:false,message:"Password must atleast 6 characters"});
-       }
-
-       const existingUserByEmail = await User.findOne({email:email})
-       
-       if(existingUserByEmail){
-        return res.status(400).json({success:false,message:"Username already exist"});
-       }
-
-       const existingUserByUsername = await User.findOne({username:username})
-
-       if(existingUserByUsername){
-        return res.status(400).json({success:false,message:"Username already exist"});
-       }
-
-       const salt = await bcryptjs.genSalt(10);
-       const hashPassword = await bcryptjs.hash(password, salt);
-
-       const PROFILE_PICS = ["/avatar.png","/avatar2.png","/avatar3.png", ]
-
-       const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)]
-
-       const newUser = new User({
-        email:email,
-        password:hashPassword,
-        username:username,
-        image,
-       });
-
-
-      
-        generateTokenAndSetCooke(newUser._id, res);
-        await newUser.save();
-        res.status(201).json({sucess:true, user:{
-            ...newUser._doc,
-            password:"" //remove password to the response
-           }});
-       
-
-  
-       
-
-
-    
-
-    } catch (error) {
-        console.log("Error in signup controller", error.message)
-        res.status(500).json({success:false, message:"Internal server error"});
+    if (!email || !password || !username) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" })
     }
 
-};
-export async function login(req,res){
-    try {
-        const {email,password} = req.body;
-        if(!email || !password){
-            return res.status(400).json({success:false, message:"All fields are required"})
-        }
-
-        const user = await User.findOne({email:email})
-        if(!user){
-           return res.status(404).json({success:false, message:"Invalid credentials"})
-        }
-
-        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
-
-        if(!isPasswordCorrect){
-            return res.status(400).json({success:false, message:"Invalid credentials"})
-        }
-
-        generateTokenAndSetCooke(user._id, res);
-
-        res.status(200).json({
-            success:true,
-            user:{
-                ...user._doc,
-                password:""
-            }
-        });
-
-
-
-    } catch (error) {
-        console.log("Error in login controller", error.message);
-        res.status(500).json({success:false, message:"Internal server error"});
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ success: false, message: "Invalid email" })
     }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Password must atleast 6 characters" })
+    }
+
+    const existingUserByEmail = await User.findOne({ email: email })
+
+    if (existingUserByEmail) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already exist" })
+    }
+
+    const existingUserByUsername = await User.findOne({ username: username })
+
+    if (existingUserByUsername) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already exist" })
+    }
+
+    const salt = await bcryptjs.genSalt(10)
+    const hashPassword = await bcryptjs.hash(password, salt)
+
+    const PROFILE_PICS = ["/avatar.png", "/avatar2.png", "/avatar3.png"]
+
+    const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)]
+
+    const newUser = new User({
+      email: email,
+      password: hashPassword,
+      username: username,
+      image,
+    })
+
+    generateTokenAndSetCooke(newUser._id, res)
+    await newUser.save()
+    res.status(201).json({
+      sucess: true,
+      user: {
+        ...newUser._doc,
+        password: "", //remove password to the response
+      },
+    })
+  } catch (error) {
+    console.log("Error in signup controller", error.message)
+    res.status(500).json({ success: false, message: "Internal server error" })
+  }
 }
-export async function logout(req,res){
-   try {
-     res.clearCookie("jwt-netflix")
-     res.status(200).json({success:true, message:"Logged out succesfully"});
-   } catch (error) {
-    console.log("Error in logout controller", error.message);
-    res.status(500).json({success:false, message:"Internal server error"});
-   }
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" })
+    }
+
+    const user = await User.findOne({ email: email })
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Invalid credentials" })
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password)
+
+    if (!isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" })
+    }
+
+    generateTokenAndSetCooke(user._id, res)
+
+    res.status(200).json({
+      success: true,
+      user: {
+        ...user._doc,
+        password: "",
+      },
+    })
+  } catch (error) {
+    console.log("Error in login controller", error.message)
+    res.status(500).json({ success: false, message: "Internal server error" })
+  }
+}
+export async function logout(req, res) {
+  try {
+    res.clearCookie("jwt-netflix")
+    res.status(200).json({ success: true, message: "Logged out succesfully" })
+  } catch (error) {
+    console.log("Error in logout controller", error.message)
+    res.status(500).json({ success: false, message: "Internal server error" })
+  }
 }
 
+export async function authCheck(req, res) {
+  try {
+    console.log("req.user", req.user)
+    res.status(200).json({ success: true, user: req.user })
+  } catch (error) {
+    console.log("Error in authCheck controller", error.message)
+    res.status(500).json({ success: false, message: "Internal server error" })
+  }
+}
